a33d7ee9e5716ec6f4523cbe7c3af402003d7155
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2025-08-10 07:21:43 -0400

    Change dynamic rendering for API route from 'force-dynamic' to 'force-static'

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-static';
 
 export async function GET() {
   return NextResponse.json({
